a67ad0004872eaad7e768fd3525b0b17fcbf4f27
TBFgunmo <gunmohome123@gmail.com>
2026-03-24 09:29:46 +0700

    UI mainmenu and other

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject manuPanel;
+    public GameObject creditPanel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void GoToGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("InGame_1"); 
+    }
+
+    public void OpenCredits()
+    {
+        creditPanel.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        creditPanel.SetActive(false);
+    }
+}
